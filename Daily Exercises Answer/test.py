c031d76b1e2563dfc4cf3195a63d84996ad7b738
Anower H. Faysal <faysalhossain8948@gmail.com>
2026-04-10 14:00:36 +0900

    Some basic commit

---
+++ Daily Exercises Answer/test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generate 1000 random numbers from uniform distribution (0 to 1)
+data = np.random.uniform(10, 50, 10000)
+
+# Plot histogram
+plt.figure()
+plt.hist(data, bins=30)
+
+# Labels and title
+plt.title("Uniform Distribution (1000 Random Numbers)")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+
+# Show plot
+plt.show()
